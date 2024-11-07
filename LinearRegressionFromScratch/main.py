c395c277584f65ffe38d5ed67b468f0d84c669bc
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-07 15:15:11 +0600

    Linear Regression From Scratch completed

--- LinearRegressionFromScratch/main.py
+++ LinearRegressionFromScratch/main.py
@@ -13,21 +13,22 @@ def gradient_descent(m_now, b_now, points, L):
         x = points.iloc[i]['X']
         y = points.iloc[i]['Y']
         
-        m_gradient += - 2 / n * x * (y - (m_now * x + b_now))
-        b_gradient += - 2 / n * (y - (m_now * x + b_now))
+        m_gradient += - (2 / n) * x * (y - (m_now * x + b_now))
+        b_gradient += - (2 / n) * (y - (m_now * x + b_now))
     
-    return m - m_gradient * L, b - b_gradient * L
+    return m_now - m_gradient * L, b_now - b_gradient * L
 
 m = 0
 b = 0
-L = 0.0001
+L = 0.00001
 epochs = 100
 
 for i in range(epochs):
     print("Epochs:", i, "m:", m, "b:", b)
     m, b = gradient_descent(m, b, df, L)
     
-print(m, b)
-plt.scatter(df['X'], df['Y'])
-plt.plot(list(range(300)), [m * x + b for x in range(300)], color="black")
+plt.scatter(df['X'], df['Y'], label='Data Points')
+x_range = range(int(df['X'].min()), int(df['X'].max()) + 1)
+plt.plot(x_range, [m * x + b for x in x_range], color="black", label='Best Fit Line')
+plt.legend()
 plt.show()
