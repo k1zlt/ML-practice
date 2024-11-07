a416fd078f0056510b1925a3c4e5d4d3a8302b59
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-07 14:53:24 +0600

    .

--- fuel_efficiency/main.py
+++ fuel_efficiency/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers # type: ignore
 import matplotlib.pyplot as plt
 
 
